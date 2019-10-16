92aef57ee9218db0f8d0e8b05411b0bdc93db424
Mark McLoughlin <markmc@redhat.com>
2019-10-16 09:40:57 +0100

    ipnet_test: add IPv6 tests

--- pkg/ipnet/ipnet_test.go
+++ pkg/ipnet/ipnet_test.go
@@ -26,6 +26,19 @@ func TestMarshal(t *testing.T) {
 	assertJSON(t, stdlibIPNet, "{\"IP\":\"192.168.0.10\",\"Mask\":\"////AA==\"}")
 	wrappedIPNet := &IPNet{IPNet: *stdlibIPNet}
 	assertJSON(t, wrappedIPNet, "\"192.168.0.10/24\"")
+
+	stdlibIPNet = &net.IPNet{
+		IP: net.IP{
+			0xfd, 0x00, 192, 168,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 10},
+		Mask: net.CIDRMask(64, 128),
+	}
+	assertJSON(t, stdlibIPNet, "{\"IP\":\"fd00:c0a8::a\",\"Mask\":\"//////////8AAAAAAAAAAA==\"}")
+	wrappedIPNet = &IPNet{IPNet: *stdlibIPNet}
+	assertJSON(t, wrappedIPNet, "\"fd00:c0a8::a/64\"")
+
 	assertJSON(t, &IPNet{}, "null")
 	assertJSON(t, nil, "null")
 }
@@ -36,6 +49,14 @@ func TestUnmarshal(t *testing.T) {
 		{IPNet: net.IPNet{
 			IP:   net.IP{192, 168, 0, 10},
 			Mask: net.IPv4Mask(255, 255, 255, 0),
+		}},
+		{IPNet: net.IPNet{
+			IP: net.IP{
+				0xfd, 0x00, 192, 168,
+				0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 10},
+			Mask: net.CIDRMask(64, 128),
 		}},
 	} {
 		t.Run(ipNetIn.String(), func(t *testing.T) {
